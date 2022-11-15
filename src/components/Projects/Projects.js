1eedf72f5c68aa029f758da5de66368a645f6b1d
veronica-christ <veronicaphago777@gmail.com>
2022-11-15 13:24:23 +0200

    Project descriptions

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,6 +10,20 @@ const Projects = () => (
   <SectionTitle>
     Projects
   </SectionTitle>
+  <GridContainer>
+    {[{title: 'Survello',
+    description:'Survello is an application that allows users to conduct surveys and for their desired targets to take those surveys'}
+  ,{title: 'Kontrak',
+  description:'Kontrak is an app that serves as a bridge between people seeking services from contractors, and contractors looking to supply on that demand. The app will help contractors to advertise their work, and service seekers to find contractors by submitting their requirements, comparing prices and reading reviews about previous services. Service seekers can also track the job as it gets done.'},
+  {title: 'Expense Tracker',
+  description:'This is a simple expense tracking app that allows users to'}
+,].map((project)=>(
+      <div>
+        {project.title }
+        {project.description}
+      </div>
+    ))}
+  </GridContainer>
 
  </Section>
 );
