d4451d11495a2a07b78a38dc12d12593cb791261
veronica-christ <veronicaphago777@gmail.com>
2022-11-15 12:39:57 +0200

    Contact me

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+<LeftSection>
+  <SectionTitle main center>
+Hi I am Veronica,<br/>
+Thank you for visiting my portfolio!
+  </SectionTitle>
+  <SectionText>
+   Here you will get to know me, learn about my experiences, abilities and skills!  
+  </SectionText>
+  <Button onClick={()=>window.location='mailto:veronicaphago777@gmail.com'}>Contact me</Button>
+ 
+</LeftSection>
+  </Section>
 );
 
 export default Hero;
