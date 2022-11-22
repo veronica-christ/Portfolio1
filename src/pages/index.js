f4f1275f9bdcc0d6098b91317df041d8f952734f
Veronica Phago <veronicaphago777@gmail.com>
2022-11-22 09:23:40 +0200

    Page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import { Section } from '../styles/GlobalComponents';
 const Home = () => {
   return (
     <Layout>
-      <title>Veronica|BackendDev</title>
+      <title>Veronica Fullstack Developer</title>
       <Section grid>
         <Hero />
         <BgAnimation />
